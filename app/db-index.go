290758d59c56b95a284991efbc0556aed28fd396
Kittiphat S <freeuxer@gmail.com>
2024-07-02 14:16:45 +0700

    introduce a method in DB-index to detect if such search can utilize index?

--- app/db-index.go
+++ app/db-index.go
@@ -3,34 +3,61 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/rqlite/sql"
 )
 
 type DBIndex struct {
 	Schema
-	db          *Db
-	forTable    *DBTable
-	indexSpec   *sql.CreateIndexStatement
-	colIndexMap map[string]int
+	db            *Db
+	forTable      *DBTable
+	indexSpec     *sql.CreateIndexStatement
+	colIndexOrder []string
 }
 
 func NewDbIndex(db *Db, schema *Schema, indexSpec *sql.CreateIndexStatement) *DBIndex {
-	var colIndexMap = map[string]int{} // columnName ~> index
-	fmt.Fprintf(os.Stderr, "[dbg] Index Spec: %s %d columns\n", indexSpec.Name.Name, len(indexSpec.Columns))
-	for d, col := range indexSpec.Columns {
-		fmt.Fprintf(os.Stderr, "[dbg]  └─COL= %s %s\n", col.String(), col.X.String())
-		colIndexMap[col.String()] = d
+	var colIndexOrder = []string{}
+	fmt.Fprintf(os.Stderr, "[dbg] Index Spec: %s %d columns for %s\n", indexSpec.Name.Name, len(indexSpec.Columns), indexSpec.Table.Name)
+	for _, col := range indexSpec.Columns {
+		fmt.Fprintf(os.Stderr, "[dbg]  └─COL= %s %s %s\n", col.X.String(), col.Asc.String(), col.Desc.String())
+		colIndexOrder = append(colIndexOrder, strings.ReplaceAll(col.X.String(), "\"", ""))
 	}
 
 	return &DBIndex{
-		db:          db,
-		indexSpec:   indexSpec,
-		colIndexMap: colIndexMap,
-		Schema:      *schema,
+		db:            db,
+		indexSpec:     indexSpec,
+		colIndexOrder: colIndexOrder,
+		Schema:        *schema,
 	}
 }
 
 func (i *DBIndex) Name() string {
 	return i.indexSpec.Name.Name
+}
+
+// Determine if this index matched the table requirement or not? If yes, how much it would help? (integer)
+// If 1 column (out of 3) matched it will return 1
+func (i *DBIndex) Match(condition *map[string]string) string {
+	// no condition being asked; this will not matched anything.
+	if len(*condition) <= 0 {
+		return ""
+	}
+	var result strings.Builder
+	for j, indexColName := range i.colIndexOrder {
+		if condVal, ok := (*condition)[indexColName]; ok == true {
+			if j > 0 {
+				result.WriteString("|")
+			}
+			result.WriteString(condVal)
+			continue
+		}
+		break
+	}
+	return result.String()
+}
+
+// Query from index
+func (i *DBIndex) SelectRange(conditionValuePrefix string) []Row {
+	return make([]Row, 0)
 }
