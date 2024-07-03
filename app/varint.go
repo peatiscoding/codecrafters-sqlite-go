1064b3e1d8403099d597c42c8390c482e405fbc0
Kittiphat S <freeuxer@gmail.com>
2024-07-03 11:11:12 +0700

    fixed varint for longer bit cases

--- app/varint.go
+++ app/varint.go
@@ -4,7 +4,6 @@ import "bytes"
 
 func ReadVarint(reader *bytes.Reader) (int64, int, error) {
 	var result int64
-	var shift uint
 	var bytesRead int
 
 	for {
@@ -16,17 +15,15 @@ func ReadVarint(reader *bytes.Reader) (int64, int, error) {
 
 		bytesRead++
 
+		result = result << 7 // FIXME: handle last byte?
+
 		// Combine the lower 7 bits into the result
-		result = result << shift
 		result |= int64(b & 0x7F)
 
 		// Check if the MSB is set; if not, we are done
 		if b&0x80 == 0 {
 			break
 		}
-
-		// Shift the result for the next 7 bits
-		shift += 7
 	}
 
 	return result, bytesRead, nil
